d9adf5d91b8fbd6d6953fd9ce2d6504867886043
JLC <JLC@192.168.1.13>
2016-02-09 22:07:20 +0000

    asyncWorker-Parallelizer

---
+++ microservices/asyncWorker-Parallelizer/src/main/java/org/xine/presentation/Index.java
@@ -0,0 +1,37 @@
+package org.xine.presentation;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import org.xine.business.push.boundary.Parallelizer;
+
+@Named
+@RequestScoped
+public class Index {
+
+	@Max(100)
+	@Min(1)
+	private int number = 10;
+
+	@Inject
+	Parallelizer parallelizer;
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(final int number) {
+		this.number = number;
+	}
+
+	public Object basicexecution() {
+		final List<Long> execute = this.parallelizer.execute(this.number);
+		return null;
+	}
+
+}
