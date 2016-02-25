6e04db99afcb6f0e1dcc53c899b1e541ee32efb4
JLC <JLC@TITAN.home>
2016-02-25 02:05:37 +0000

    Binary REST Serializer

---
+++ microservices/binary-REST-Serializer/src/main/java/org/xine/bookshop/business/books/entity/Book.java
@@ -0,0 +1,51 @@
+package org.xine.bookshop.business.books.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long imdb;
+	private String name;
+
+	protected Book() {
+	}
+
+	public Book(final Long imdb, final String name) {
+		this.imdb = imdb;
+		this.name = name;
+	}
+
+	public Long getImdb() {
+		return this.imdb;
+	}
+
+	public void setImdb(final Long imdb) {
+		this.imdb = imdb;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [imdb=" + this.imdb + ", name=" + this.name + "]";
+	}
+
+}
---
+++ microservices/binary-REST-Serializer/src/test/java/org/xine/bookshop/business/books/boundary/BookResourceIT.java
@@ -0,0 +1,46 @@
+package org.xine.bookshop.business.books.boundary;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.xine.bookshop.business.books.entity.Book;
+import org.xine.bookshop.business.restserialization.control.CustomMediaType;
+import org.xine.bookshop.business.restserialization.control.JavaDeserializer;
+import org.xine.bookshop.business.restserialization.control.JavaSerializer;
+
+public class BookResourceIT {
+
+	private static final String RESOURCE_URI = "http://localhost:8080/binary-REST-Serializer/resources";
+
+	private Client client;
+
+	@Before
+	public void init() {
+		this.client = ClientBuilder.newClient();
+		this.client.register(JavaSerializer.class)
+		.register(JavaDeserializer.class);
+	}
+
+	@Test
+	public void get() {
+		final WebTarget target = this.client.target(RESOURCE_URI).path("book");
+		final Book book = target.request(CustomMediaType.SERIALIZATION_JAVA).get(Book.class);
+
+		Assert.assertNotNull(book);
+	}
+
+	@Test
+	public void post() {
+		final Response post = this.client.target(RESOURCE_URI).path("book")
+				.request(CustomMediaType.SERIALIZATION_JAVA)
+				.post(Entity.entity(new Book(123L, "xyz"), CustomMediaType.SERIALIZATION_JAVA));
+
+	}
+
+}
