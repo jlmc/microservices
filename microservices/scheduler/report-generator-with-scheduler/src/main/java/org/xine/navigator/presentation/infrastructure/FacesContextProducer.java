129c97baffceb81d0130c47cedaa911e0cdbbf61
JLC <JLC@TITAN.home>
2016-02-29 01:39:17 +0000

    fix the default cdi scope

--- microservices/scheduler/report-generator-with-scheduler/src/main/java/org/xine/navigator/presentation/infrastructure/FacesContextProducer.java
+++ microservices/scheduler/report-generator-with-scheduler/src/main/java/org/xine/navigator/presentation/infrastructure/FacesContextProducer.java
@@ -1,12 +1,16 @@
 package org.xine.navigator.presentation.infrastructure;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.faces.context.FacesContext;
 
 @ApplicationScoped
-public class FacesContextProducer {
+public class FacesContextProducer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Produces
 	@RequestScoped
