32cf1a48154b577443a6f31c7da25e318987cf9c
costa <costa@xine.home>
2016-04-04 23:03:30 +0100

    crudservice

---
+++ microservices/plugins/xebuy/src/main/java/org/xine/xebuy/business/CrudService.java
@@ -0,0 +1,65 @@
+package org.xine.xebuy.business;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.MANDATORY)
+public class CrudService {
+
+	@PersistenceContext
+	EntityManager em;
+
+	public <T> T create(T t) {
+		em.persist(t);
+		em.flush();
+		this.create(t);
+		return t;
+	}
+
+	public <T> T find(Class<T> type, Object id) {
+		return em.find(type, id);
+	}
+
+	public void delete(Class type, Object id) {
+		final Object reference = em.getReference(type, id);
+		em.remove(reference);
+	}
+
+	public <T> T update(T t) {
+		return em.merge(t);
+	}
+
+	public List findWithNamedQuery(String namedQueryName) {
+		return em.createNamedQuery(namedQueryName).getResultList();
+	}
+
+	public List findWithNamedQuery(String namedQueryName, Map<String, Object> parameters) {
+		return this.findWithNamedQuery(namedQueryName, parameters, 0);
+	}
+
+	public List findWithNamedQuery(String namedQueryName, Map<String, Object> parameters, int resultLimit) {
+		final Query query = em.createNamedQuery(namedQueryName);
+		final Set<java.util.Map.Entry<String, Object>> rawParameters = parameters.entrySet();
+
+		if (resultLimit > 0) {
+			query.setMaxResults(resultLimit);
+		}
+
+		rawParameters.forEach(e -> query.setParameter(e.getKey(), e.getValue()));
+
+		return query.getResultList();
+	}
+
+	public <T> List<T> findByNativeQuery(String sql, Class<T> type) {
+		return em.createNativeQuery(sql, type).getResultList();
+	}
+}
